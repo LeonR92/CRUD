0692a93bbef071bf4ea9f592a6cd3e28e23b33e7
LeonR92 <127194165+LeonR92@users.noreply.github.com>
2024-01-20 11:29:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app.config['CACHE_TYPE'] = 'filesystem'
 app.config['CACHE_DIR'] = '.'  
 cache = Cache(app)
 
-# Register the Blueprint with the Flask application
+# Path todo
 app.register_blueprint(financiallines,url_prefix='/financiallines')
 
 if __name__ == '__main__':
